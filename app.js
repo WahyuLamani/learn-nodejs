84b282630614a95bc58f77ffcf2d04f800555553
wahyulamani <wahyu@elektro.polimdo.ac.id>
2021-09-28 14:10:27 +0800

    tambah view enggine. buat tampilan home

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
 const port = 3000;
 
 /**template using EJS */
-
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    const data = {
+        nama: "wahyu Lamani",
+        umur: 21,
+        alamat: 'bailang lingkungan 6',
+        email: 'razorwahyu1@gmail.com'
+    }
+    // console.log(data);
+
+    res.render('home', {
+        title: 'halaman Home',
+        layout: 'layouts/core',
+        data: data
+    });
 });
 
 
